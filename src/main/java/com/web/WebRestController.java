4b397beead07afb907975e88efee239272b6ba9b
황지혜 <jh.hwang@crscube.co.kr>
2019-08-22 12:31:45 +0900

    first commit

---
+++ src/main/java/com/web/WebRestController.java
@@ -0,0 +1,14 @@
+package com.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// @ResponseBody를 모든 메소드에서 적용해준다.
+@RestController
+public class WebRestController {
+    // hello 메소드의 결과로 "HelloWrold"라는 문자열을 JSON 형태로 반환한다.
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+}
