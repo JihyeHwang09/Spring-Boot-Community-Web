10f22e5862f163f758011ed2135f73491687ed9e
JihyeHwang09 <raye9053@gmail.com>
2019-09-09 17:20:15 +0900

    HandlerMethodArgumentResolver 인터페이스를 구현한 UserArgumentResolver 클래스 생성

--- src/main/java/com/web/controller/LoginController.java
+++ src/main/java/com/web/controller/LoginController.java
@@ -1,16 +1,8 @@
 package com.web.controller;
 
 import com.web.domain.User;
-import com.web.domain.enums.SocialType;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpSession;
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 // 인증된 User 정보를 세션에 저장해주는 기능 생성
 @Controller
@@ -25,33 +17,13 @@ public class LoginController {
 인증이 성공적으로 처리된 이후에 리다이렉트되는 경로
 허용하는 요청의 URL 매핑을 /facebook/complete, /google/complete, /kakao/complete로 제한한다.
 */
-
     @GetMapping (value="/{facebook | google | kakao}/complete")
-    public String loginComplete(HttpSession session) {
-        /*
-        SecurityContextHolder에서 인증된 정보를 OAuth2Authentication 형태로 받아온다.
-        OAuth2Authentication은 기본적인 인증에 대한 정보뿐만 아니라,
-        OAuth2 인증과 관련된 정보도 함께 제공한다.
-         */
-        OAuth2Authentication authentication = (OAuth2Authentication)
-                SecurityContextHolder.getContext().getAuthentication();
-                /*
-                리소스 서버에서 받아온 개인 정보를 getDetails()를 사용해 Map 타입으로 받을 수 있다.
-                 */
-        @SuppressWarnings("unchecked")
-        Map<String, String> map = (HashMap <String, String>)
-                authentication.getUserAuthentication().getDetails();
-                /*
-                세션에 빌더를 사용하여 인증된 User 정보를 User 객체로 변환하여 저장한다.
-                */
-        session.setAttribute("user", User.builder()
-        .name(map.get("name"))
-        .email(map.get("email"))
-        .principal(map.get("id"))
-        .socialType(SocialType.FACEBOOK)
-        .createdDate(LocalDateTime.now())
-        .build()
-        );
+    /*
+    @SocialUser User user 형식의 간단한 방법으로 인증된 User 객체를 가져올 수 있다.
+    -> 코드의 재사용성에 있어서도 큰 이득을 얻게 된다.
+    단지, 컨트롤러의 파라미터에 @SocialUser 어노테이션이 있고 타입이 User면 된다.
+     */
+    public String loginComplete(@SocialUser User user) {
         return "redirect:/board/list";
     }
 }
