e21d340dfa1ece8ee1150dd8063dc73a5cf1d035
JihyeHwang09 <raye9053@gmail.com>
2019-09-09 17:36:21 +0900

    HandlerMethodArgumentResolver 인터페이스를 구현한 UserArgumentResolver 클래스 생성

--- src/main/java/com/web/resolver/UserArgumentResolver.java
+++ src/main/java/com/web/resolver/UserArgumentResolver.java
@@ -1,11 +1,14 @@
 package com.web.resolver;
 
 import org.springframework.core.MethodParameter;
+
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Component
 public class UserArgumentResolver implements HandlerMethodArgumentResolver {
 
     public boolean supportsParameter(MethodParameter parameter) {
